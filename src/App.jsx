633c7671875bcebf843803292d95deaf3dc8ed44
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-06-02 15:10:10 +0300

    Initialize components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { useState } from 'react';
-import './App.css';
+import { Nav } from './components/Nav.jsx';
+import { Logo } from './components/Logo.jsx';
+import { ImageLinkForm } from './components/ImageLinkForm.jsx';
+import { FaceRecognitionContainer } from './components/FaceRecognitionContainer.jsx';
 
 function App() {
-  return;
+  const [state, setState] = useState(null);
+  return (
+    <div className="App">
+      <Nav />
+      <Logo />
+      <ImageLinkForm />
+      <FaceRecognitionContainer />
+    </div>
+  );
 }
 
 export default App;
