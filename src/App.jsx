954d7218dd9ac6bf3b093670e771dbfc8ded6a3b
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-06-02 15:37:38 +0300

    Install tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 import { Nav } from './components/Nav.jsx';
-import { Logo } from './components/Logo.jsx';
-import { ImageLinkForm } from './components/ImageLinkForm.jsx';
-import { FaceRecognitionContainer } from './components/FaceRecognitionContainer.jsx';
+// import { Logo } from './components/Logo.jsx';
+// import { ImageLinkForm } from './components/ImageLinkForm.jsx';
+// import { FaceRecognitionContainer } from './components/FaceRecognitionContainer.jsx';
 
 function App() {
   const [state, setState] = useState(null);
   return (
     <div className="App">
       <Nav />
-      <Logo />
+      {/* <Logo />
       <ImageLinkForm />
-      <FaceRecognitionContainer />
+      <FaceRecognitionContainer /> */}
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
     </div>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Nav() {
+  return (
+    <nav className="container mx-auto flex flex-col">
+      <p>Sign Out</p>
+    </nav>
+  );
+}
