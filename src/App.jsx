c2e267a21484a6cc7b1be18bd94bf00a728a0687
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-06-04 12:40:35 +0300

    Fetch the rigth data from clarifai

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,47 @@ function App() {
 
   const onSubmit = value => {
     console.log(value);
+    // URL of image to use. Change this to your image.
+    const IMAGE_URL = urlInput;
+
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: 'nakata',
+        app_id: 'face-detect-app',
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: IMAGE_URL,
+            },
+          },
+        },
+      ],
+    });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Key 99ffb1859828459aa26dea23b7c089df',
+      },
+      body: raw,
+    };
+
+    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+    // this will default to the latest version_id
+
+    fetch(
+      `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+
+    // ENDS HERE
   };
 
   return (
