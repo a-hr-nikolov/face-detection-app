0b1adc56fde148048cde2db6946bb5e81e51b072
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-06-04 11:52:28 +0300

    Link App to ImageLinkForm

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,26 @@ import { Particle } from './components/Particle/Particle.jsx';
 // import { FaceRecognitionContainer } from './components/FaceRecognitionContainer.jsx';
 
 function App() {
-  const [state, setState] = useState(null);
+  const [urlInput, setUrlInput] = useState('');
+
+  const onUrlInputChange = value => {
+    setUrlInput(() => value);
+  };
+
+  const onSubmit = value => {
+    console.log(value);
+  };
+
   return (
     <div className="App">
       <div className="contents">
         <Nav />
         <Logo />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onChange={onUrlInputChange}
+          onSubmit={onSubmit}
+          urlValue={urlInput}
+        />
         {/*
         <FaceRecognitionContainer /> */}
       </div>
--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-export function ImageLinkForm() {
+export function ImageLinkForm({ onChange, onSubmit, urlValue }) {
+  const handleChange = ({ target }) => {
+    onChange(target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    onSubmit('some value');
+  };
+
   return (
     <div className="form-container flex flex-col gap-4 items-center justify-center pt-4">
       <h1 className="text-4xl">Detect-A-Facer</h1>
@@ -10,17 +19,21 @@ export function ImageLinkForm() {
         to think humans are born with this amazing capability? Astounding!
       </p>
       <div className="input-form flex flex-col sm:flex-row justify-center gap-4 self-stretch sm:rounded-full rounded-3xl">
-        <input
-          type="text"
-          className="url-input flex-grow px-3 sm:rounded-full rounded-xl focus:outline-none py-2"
-          placeholder="Enter image URL"
-        />
-        <button
-          type="button"
-          className="search-btn bg-sky-600 text-white px-12 py-2 sm:rounded-full rounded-xl hover:scale-105 active:scale-110"
-        >
-          Detect
-        </button>
+        <form onSubmit={handleSubmit} className="contents">
+          <input
+            type="text"
+            className="url-input flex-grow px-3 sm:rounded-full rounded-xl focus:outline-none py-2"
+            placeholder="Enter image URL"
+            value={urlValue}
+            onChange={handleChange}
+          />
+          <button
+            type="submit"
+            className="search-btn bg-sky-600 text-white px-12 py-2 sm:rounded-full rounded-xl hover:scale-105 active:scale-110"
+          >
+            Detect
+          </button>
+        </form>
       </div>
     </div>
   );
