e7ac4a5203a729ff82be81a4ec619e3bdebcad5a
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-06-12 08:43:45 +0300

    Move Clarifai to the server

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   const SERVER_URL = 'http://localhost:3000';
 
   function processBoxData(data) {
-    const boundingBoxes = data.outputs[0].data.regions;
+    const boundingBoxes = data.regions;
 
     if (isSignedIn) increaseUserDetected(boundingBoxes.length);
 
@@ -71,58 +71,17 @@ function App() {
     setUrlInput(() => value);
   }
 
-  function onSubmit() {
+  async function onSubmit() {
     document.querySelector('#face-rec-container').classList.remove('hidden');
     setUrlOutput(urlInput);
-
-    const raw = JSON.stringify({
-      user_app_id: {
-        user_id: 'nakata',
-        app_id: 'face-detect-app',
-      },
-      inputs: [
-        {
-          data: {
-            image: {
-              url: urlInput,
-            },
-          },
-        },
-      ],
-    });
-
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        Authorization: 'Key 99ffb1859828459aa26dea23b7c089df',
-      },
-      body: raw,
-    };
-
-    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
-    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
-    // this will default to the latest version_id
-    // `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
-
-    fetch(
-      `https://api.clarifai.com/v2/models/face-detection/outputs`,
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => {
-        setFaceBoxData(processBoxData(result));
-        console.log(result);
-        // let faceBoxData = result.outputs[0].data.regions;
-        // processBoxData(faceBoxData);
-        // faceBoxData.forEach(item => console.log(item.region_info.bounding_box));
-        //processBoxData(faceBoxData);
-      })
-      .catch(error => {
-        console.log('error, but will call the other thing', error);
-      });
-
-    // ENDS HERE
+    try {
+      const result = await fetch(SERVER_URL + '/detect?url=' + urlInput);
+      const data = await result.json();
+      setFaceBoxData(processBoxData(data));
+      setUrlInput('');
+    } catch (err) {
+      console.log('Error fetching, try again');
+    }
   }
 
   function signOut() {
