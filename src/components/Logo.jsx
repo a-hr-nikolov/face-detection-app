6c038942c256113caf14297c0a91ad5a3d41ae79
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-06-12 11:03:51 +0300

    Update logo path

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Logo() {
   return (
     <img
-      src="/logo.png"
+      src="logo.png"
       className="block mx-auto my-0 w-[min(30%,10rem)]"
       alt="logo"
     />
