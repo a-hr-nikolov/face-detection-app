7a299fd88bc3790b5468d37eb4df6c801db66c02
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-06-14 15:04:58 +0300

    Create button

---
+++ src/components/GuestButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function GuestButton({ setRoute }) {
+  return (
+    <button
+      className="block w-full bg-gray-500 hover:bg-gray-600 active:scale-105 text-white font-bold py-2 px-4 mt-8 rounded focus:outline-none focus:shadow-outline"
+      type="button"
+      onClick={() => setRoute('home')}
+    >
+      Use as guest
+    </button>
+  );
+}
